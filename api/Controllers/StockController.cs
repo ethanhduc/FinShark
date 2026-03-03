ab466bc0528cc05fcd58e7745f81c6527de05191
Ethan H. Nguyen <ethanhducnguyen@gmail.com>
2026-03-02 23:35:54 -0600

    created Update method

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using api.Data;
 using api.DTOs.Stock;
 using api.Mappers;
+using api.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
@@ -46,6 +48,29 @@ namespace api.Controllers
             _context.Stocks.Add(stockModel); // add to database context
             _context.SaveChanges(); // save changes to database (id generated here)
             return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDTO()); // return 201 with location header pointing to new resource
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDTO updateDTO)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id); // find stock by id
+
+            if(stockModel == null)
+            {
+                return NotFound();
+            }
+
+            stockModel.Symbol = updateDTO.Symbol;
+            stockModel.CompanyName = updateDTO.CompanyName;
+            stockModel.Purchase = updateDTO.Purchase;
+            stockModel.LastDiv = updateDTO.LastDiv;
+            stockModel.Industry = updateDTO.Industry;
+            stockModel.MarketCap = updateDTO.MarketCap;
+
+            _context.SaveChanges();
+
+            return Ok(stockModel.ToStockDTO());
         }
     }
 }
